80c00115b7c1877d37dc7f9191e21d644116ee10
CLD <531987793@qq.com>
2018-08-22 23:00:54 +0800

    8/22 继续做题

--- test.py
+++ test.py
@@ -1,31 +1,9 @@
-n=2
-m=1
-wang= [[0 for col in range(n+2)] for row in range(m+2)]
-mi=100000
-def yima(x,y,n,m,b):
-    global mi
-    global wang
-    if x<0 or x>n or y<0 or y>m:
-        return
-    if x==n and y==m:
-        wang[x][y]=1
-        if b<mi:
-            mi=b
-        return
-    if wang[x][y]==1:
-        return
-    wang[x][y]=1
-    yima(x+2,y+1,n,m,b+1)
-    yima(x+2,y-1,n,m,b+1)
-    yima(x-2,y+1,n,m,b+1)
-    yima(x-2,y-1,n,m,b+1)
-    yima(x + 1, y + 2, n, m, b + 1)
-    yima(x + 1, y - 2, n, m, b + 1)
-    yima(x - 1, y + 2, n, m, b + 1)
-    yima(x - 1, y - 2, n, m, b + 1)
+L=[2,-3,3,50]
 
-yima(0,0,n,m,0)
-if mi==100000:
-    print("-1")
-else:
-    print(mi)
+p=[0]*len(L)
+p[0]=L[0]
+for i in range(1,len(L)):
+	p[i]=max(L[i]+p[i-1],L[i])
+print(max(p))
+
+
